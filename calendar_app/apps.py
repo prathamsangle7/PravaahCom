fcd9193f3bd2cc1dae5cf568a25da0422f64c379
prathamsangle7 <prathamsangle5@gmail.com>
2026-06-05 12:07:39 +0530

    Delete calendar_app directory

--- calendar_app/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CalendarAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'calendar_app'
-    verbose_name = 'Calendar'
